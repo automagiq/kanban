b144b140c8d99879624793df771ae1854ac625bd
automagiq <geko.tm@gmail.com>
2020-08-11 23:17:11 -0600

    creating Kanban App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Column from './Column';
+import {Container, Row} from "reactstrap";
+import Controller from "./Controller";
+
 
 function App() {
+
+    const taskList = [
+        {id:Math.random(), name:"Task One", priority:0, status: "todo"},
+        {id:Math.random(), name:"Task Two", priority:1, status: "todo"},
+        {id:Math.random(), name:"Task Three", priority:1, status: "todo"},
+        {id:Math.random(), name:"Task Four", priority:2, status: "todo"}
+        ]
+
+    const columnList = [
+        {id:Math.random(), title:"To DO",  status: "todo"},
+        {id:Math.random(), title:"In Progress Two",  status: "progress"},
+        {id:Math.random(), title:"Review",  status: "review"},
+        {id:Math.random(), title:"D O N E ",  status: "done"}
+        ]
+
+
+const statuses = ['todo', 'progress', 'review', 'done'];
+const taskPriority =[0,1,2];
+
+    const [tasks, setTasks] = useState(taskList);
+
+
+    const changeTaskStatus = (taskId, direction)=> {
+        const newTask =tasks.map(el=>{
+            if (el.id===taskId) {
+                if (direction==='right') el.status=statuses[statuses.indexOf(el.status)+1]
+                if (direction==='left') el.status=statuses[statuses.indexOf(el.status)-1]
+                 if (direction==='left') el.status=statuses[statuses.indexOf(el.status)-1]
+                if (direction==='up') el.priority=taskPriority[taskPriority.indexOf(el.priority)+1]
+                if (direction==='down') el.priority=taskPriority[taskPriority.indexOf(el.priority)-1]
+
+
+            }
+            return el
+        })
+    setTasks(newTask)
+    }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+ <Container>
+
+ <Controller/>
+<Row>
+
+
+
+    {columnList.map(el=> <Column tasks={tasks} column={el}  changeTaskStatus={changeTaskStatus}/>)}
+            {/*<Column changeTaskStatus={changeTaskStatus} title={'todo'} tasks={tasks}/>*/}
+            {/*<Column changeTaskStatus={changeTaskStatus} title={'progress'} tasks={tasks}/>*/}
+            {/*<Column changeTaskStatus={changeTaskStatus} title={'review'} tasks={tasks}/>*/}
+            {/*<Column changeTaskStatus={changeTaskStatus} title={'done'} tasks={tasks}/>*/}
+ </Row>
+
+
+
+ </Container>
+
+
     </div>
   );
 }
